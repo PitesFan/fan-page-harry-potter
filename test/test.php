368692b015d0e40e9038ddfce26da419bbe0b736
PitesFan <valeriacroitoru03@gmail.com>
2026-05-26 11:23:43 +0300

    added test folder

--- test/test.php
+++ test/test.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-echo "hello world";
+echo "<h1>Salut din PHP!</h1>
+<h2>Acesta este un mesaj afișat în browser folosind PHP.</h2>
+<script>console.log('Salut din consola JavaScript!');</script>";
 ?>
-
-</body>
-</html>
